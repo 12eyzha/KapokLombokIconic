643b5c1dbe07e1542bb7b1a280b5776301631520
12eyzha <Rayhanbagas27@gmail.com>
2026-06-15 17:52:06 +0700

    Fix image imports

--- src/data/Products.js
+++ src/data/Products.js
@@ -1,24 +1,36 @@
+import igaBakar from "../assets/Menu/Igabakar.jpg";
+import ayamGoreng from "../assets/Menu/Ayamgoreng.jpg";
+import bebekGoreng from "../assets/Menu/Bebekgoreng.jpg";
+
+import lele from "../assets/Menu/Lele.jpg";
+import ikanBakar from "../assets/Menu/Ikanbakar.jpg";
+import ikanAsap from "../assets/Menu/Ikanasap.jpg";
+
+import tempeBacem from "../assets/Menu/Tempebacem.jpg";
+import tahuSusu from "../assets/Menu/Tahususu.jpg";
+import sayurKangkung from "../assets/Menu/Sayurkangkung.jpg";
+import nasi from "../assets/Menu/Nasi.jpg";
 const menuData = {
   menuUtama: [
     {
       id: 1,
       name: "Iga Goreng",
       price: "Rp 27.000",
-      image: "/src/assets/Menu/Igabakar.jpg",
+      image: igaBakar,
       rating: 4.8,
     },
     {
       id: 2,
       name: "Ayam Goreng",
       price: "Rp 19.000",
-      image: "/src/assets/Menu/Ayamgoreng.jpg",
+      image: ayamGoreng,
       rating: 4.8,
     },
     {
       id: 3,
       name: "Bebek Goreng",
       price: "Rp 23.000",
-      image: "/src/assets/Menu/Bebekgoreng.jpg",
+      image: bebekGoreng,
       rating: 4.8,
     },
   ],
@@ -26,23 +38,23 @@ const menuData = {
   menuIkan: [
     {
       id: 4,
-      name: "Lele Crispy ",
+      name: "Lele Crispy",
       price: "Rp 12.000",
-      image: "/src/assets/Menu/Lele.jpg",
+      image: lele,
       rating: 4.8,
     },
     {
       id: 5,
       name: "Ikan Mujair",
       price: "Rp 14.000",
-      image: "/src/assets/Menu/Ikanbakar.jpg",
+      image: ikanBakar,
       rating: 4.8,
     },
     {
       id: 6,
       name: "Ikan Asap",
       price: "Rp 12.000",
-      image: "/src/assets/Menu/Ikanasap.jpg",
+      image: ikanAsap,
       rating: 4.8,
     },
   ],
@@ -52,28 +64,28 @@ const menuData = {
       id: 7,
       name: "Tempe Bacem",
       price: "Rp 10.000",
-      image: "/src/assets/Menu/Tempebacem.jpg",
+      image: tempeBacem,
       rating: 4.8,
     },
     {
       id: 8,
       name: "Tahu Susu",
       price: "Rp 7.000",
-      image: "/src/assets/Menu/Tahususu.jpg",
+      image: tahuSusu,
       rating: 4.8,
     },
     {
       id: 9,
       name: "Sayur Kangkung",
       price: "Rp 10.000",
-      image: "/src/assets/Menu/Sayurkangkung.jpg",
+      image: sayurKangkung,
       rating: 4.8,
     },
     {
       id: 10,
       name: "Nasi",
       price: "Rp 3.000",
-      image: "/src/assets/Menu/Nasi.jpg",
+      image: nasi,
       rating: 4.8,
     },
   ],
